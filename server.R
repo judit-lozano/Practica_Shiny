210344561d28e6a5db098ed5ff6e600dd0da6667
Judit <juditgoes@hotmail.com>
2022-01-26 19:08:44 +0100

    added server + ui

---
+++ server.R
@@ -0,0 +1,54 @@
+library(shiny)
+library(ggplot2)
+
+shinyServer(#una funcion dentro de shinyServer
+  function(input, output) {
+    
+    funcion_base <- function(num, chosen, strategy){
+      doors<-1:num
+      guess<-chosen
+      prize<-floor(runif(1,1,(num+1))) #randomize which door has the good prize
+      ## Reveal one of the doors you didn't pick which has a bum prize
+      if(prize!=guess){
+        reveal<-doors[-c(prize,guess)]
+      }else{
+        reveal<-sample(doors[-c(prize,guess)],length(doors)-2)
+      }
+      ## Stay with your initial guess or switch
+      if(strategy=='switch'){
+        select<-doors[-c(reveal,guess)]
+      }else{
+        select<-guess
+      }
+      # resultado
+      if(select==prize)
+      {
+        outcome<-'Ganas el premio!'
+      }else{
+        outcome<-'Pierdes!'
+      }
+      
+      return(cat(paste('Numero de puertas para jugar: ',num,
+                       '\nPuerta elegida al principio: ',guess,
+                       '\nEstrategia escogida: ',strategy,
+                       '\nPuerta elegida al final: ',select,
+                       '\nPuerta con Premio: ',prize,
+                       '\n',outcome,'\n\n',sep='')))
+    }
+    
+    #Test
+    #Si yo elijo:
+    
+    num <- 3#(input$sample_size) #numero de puertas
+    chosen <- 1 #puerta elegida
+    strategy <-'swithch'#(input$vary) #'switch' or 'stay'
+    
+    funcion_base(num, chosen, strategy)   
+    
+    output$summary <- renderPrint({ #genero el sumario descriptivo que se actualiza según se actualice el set de datos mybox$df
+      #y se traslada al UI a traves del ID "summary"
+      funcion_base(num, chosen, strategy)
+    })
+ 
+  }
+)
---
+++ ui.R
@@ -0,0 +1,22 @@
+library(shiny)
+library(ggplot2)
+
+variables = list("switch"="switch", "stay"="stay")
+
+#estructura jerárquica que empieza con shinyUI
+shinyUI(fluidPage(
+  titlePanel("Monty Hall Paradox"), #hijo de fuidPage
+  sidebarLayout(#hijo de fuidPage
+    sidebarPanel(#hijo de sidebarLayout
+      numericInput('sample_size', 'Numero de puertas',
+                   3, min = 1, max = 10000),#hijo de sidebarPanel, aquí se le pide al usuario un input numérico, en este caso que elija el tamaño de la muestra.
+      selectInput("vary", #aquí se le pide al usuario un input de seleccionar, en este caso que elija el filtrado de cut para el que quiere ver el gráfico.
+                  label="Estrategia",
+                  choices=variables)#hijo de sidebarPanel
+    ),
+    mainPanel(
+      verbatimTextOutput("summary") #aqui quiero colocar el gráfico "graph" que genero desde mi server file.
+    )
+  )
+)
+)
