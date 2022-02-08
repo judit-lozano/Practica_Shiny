46b0da68b02f2f4096f24646a1fe5efa7cec38d5
Judit <juditgoes@hotmail.com>
2022-02-08 20:19:51 +0100

    updated files

--- ui.R
+++ ui.R
@@ -1,24 +1,36 @@
 library(shiny)
 library(ggplot2)
+library(shinyWidgets)
 
-variables = list("Cambiar la puerta"="switch", "Mantener la puerta"="stay")
 
 #estructura jerárquica que empieza con shinyUI
 shinyUI(fluidPage(
-  titlePanel("Monty Hall Paradox"), #hijo de fuidPage
-  sidebarLayout(#hijo de fuidPage
-    sidebarPanel(#hijo de sidebarLayout
-      numericInput('sample_size', 'Numero de puertas',
-                   3, min = 3, max = 10000),#hijo de sidebarPanel, aquí se le pide al usuario un input numérico, en este caso que elija el tamaño de la muestra.
-      numericInput('choice', 'Elige una puerta para empezar',
-                   3, min = 1, max = 10000),
-      selectInput("vary", #aquí se le pide al usuario un input de seleccionar, en este caso que elija el filtrado de cut para el que quiere ver el gráfico.
-                  label="Estrategia",
-                  choices=variables)#hijo de sidebarPanel
-    ),
-    mainPanel(
-      verbatimTextOutput("summary") #aqui quiero colocar el gráfico "graph" que genero desde mi server file.
-    )
+  titlePanel("Monty Hall Paradox"), 
+  tabsetPanel(
+              tabPanel("Single Experiment", fluidRow(
+                # A column of 3 and column of 9
+                # 3 is for buttons and 9 for rendering images
+                column(width = 3,
+                       # conditional panels change the UI for various points in the game
+                       conditionalPanel( "input.select % 3 == 0",
+                                         selectInput("selectDoor", label = "Select Door", 
+                                                     choices = list("Door 1" = "1", "Door 2" = "2", "Door 3" = "3"))
+                       ), conditionalPanel( "input.select % 3 == 1", 
+                                            htmlOutput("current"),
+                                            selectInput("stay", label = "Select Strategy",
+                                                        choices = list("Stay" = TRUE, "Switch" = FALSE))
+                       ), conditionalPanel("input.select % 3 == 2", 
+                                           htmlOutput("winner")
+                       ), actionButton("select", "Continue")
+                ),
+                column(width = 9, htmlOutput("doors"))
+              )),
+              tabPanel("Multiple Experiment", fluidRow(
+                column(width = 4,
+                       textOutput("monte_about"),
+                       sliderInput("n_iter", label = "Number of Trials", min = 1,
+                                   max = 200, value = 25)),
+                column(width = 8, plotOutput("Monte_Carlo"))
+              ))
   )
-)
-)
+))
